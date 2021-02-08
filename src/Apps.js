6c11e0246d8cf5df4e8c5983bea4bb0a3b91ee35
RM <70502707+reyad0606@users.noreply.github.com>
2021-02-08 18:21:12 -0500

    Done

---
+++ src/Apps.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+function App() {
+  let [responseData, setResponseData] = React.useState('');
+  const fetchData = React.useCallback(() => {
+    axios({
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/todos?',
+      headers: {
+        'content-type': 'application/octet-stream',
+        'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+      },
+      params: {
+        language_code: 'en',
+      },
+    })
+      .then((response) => {
+        setResponseData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  React.useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Fetching Data with React Hooks</h1>
+        <button type='button' onClick={fetchData}>
+          Click for Data
+        </button>
+      </header>
+      <main>
+        {responseData && (
+          <blockquote>
+            "{responseData && responseData.content}"
+            <small>
+              {responseData &&
+                responseData.originator &&
+                responseData.originator.name}
+            </small>
+          </blockquote>
+        )}
+      </main>
+      {/* <pre>
+        <code>
+          {responseData && JSON.stringify(responseData, null, 4)}
+        </code>
+      </pre> */}
+    </div>
+  );
+}
+export default App;
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import ListData from './ListData';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Todo List Data Loading by button click</h1>
+      <ListData />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ListData.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
+function ListData() {
+  const [todoList, setTodoList] = useState([]);
+  const [start, setStart] = useState(0);
+
+  const fetchData = useCallback(() => {
+    axios({
+      method: 'GET',
+      url: 'https://jsonplaceholder.typicode.com/todos?',
+      params: {
+        _start: start,
+        _limit: 10,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        setTodoList(response.data);
+        // setStart(`${_start} + ${_limit}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <div>
+      <ul>
+        {todoList.map((list) => (
+          <li key={list.id} className='listdata'>
+            Todo: {list.id} --- {list.title}
+            {/* {setStart(list.id)} */}
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchData} className='button'>
+        Click to Load More
+      </button>
+    </div>
+  );
+}
+
+export default ListData;
